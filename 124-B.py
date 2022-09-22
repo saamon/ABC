1ab308a3d20df08c75f7b96f92fc3eef287ac168
saamon <suzuken2960@gmail.com>
2022-09-23 00:55:41 +0900

    124-B.py

--- 124-B.py
+++ 124-B.py
@@ -1,12 +1,12 @@
 n = int(input())
 h = list(map(int, input().split()))
 
-tmp = -1  # 一時的に値を保持する
+tmp_max = -1
+count = 0
 
-ans = 0
-
-for i in h:
-    if i >= tmp:
-        ans += 1
-        tmp = i  # もし超えていたら入れ替える
-print(ans)
+# 前よりも高ければ値を更新する
+for i in range(n):
+    if h[i] >= tmp_max:
+        count += 1
+        tmp_max = h[i]
+print(count)
