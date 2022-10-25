da32fbd3eb73f79040f28e5ea8eaa3fa2d9de302
saamon <suzuken2960@gmail.com>
2022-10-26 01:02:29 +0900

    244-B.py

---
+++ 244-B.py
@@ -0,0 +1,15 @@
+N = int(input())
+T = input()
+
+DX = [1, 0, -1, 0]
+DY = [0, -1, 0, 1]
+
+x, y = 0, 0
+d = 0  # 現在の向き
+for t in T:
+    if t == "S":
+        x += DX[d]
+        y += DX[d]
+    else:
+        d = (d + 1) % 4  # 3の次は0なので4で割った余りを取る
+print(x, y)
