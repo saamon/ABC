776c09b22d5d53757d98f905c9f24d8661e6faf0
Kento Suzuki <suzuken2960@gmail.com>
2023-01-04 18:12:07 +0900

    正規表現（白昼夜）

---
+++ code/133-B.py
@@ -0,0 +1,15 @@
+# abc133
+import math
+
+n, d = map(int, input().split())
+x = [list(map(int, input().split())) for _ in range(n)]
+cnt = 0
+
+for i in range(n):
+    for j in range(i + 1, n):
+        ans = 0
+        for k in range(d):
+            ans += (x[i][k] - x[j][k]) ** 2
+        if math.sqrt(ans).is_integer():
+            cnt += 1
+print(cnt)
