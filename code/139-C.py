b4804514cae964e68c68415530799e9b77c2eae9
Kento Suzuki <suzuken2960@gmail.com>
2023-01-11 00:52:53 +0900

    139-C.py

---
+++ code/139-C.py
@@ -0,0 +1,13 @@
+N = int(input())
+H = list(map(int, input().split()))
+ans = 0
+cnt = 0
+
+for i in range(N - 1):
+    if H[i] >= H[i + 1]:
+        cnt += 1
+    else:
+        ans = max(cnt, ans)
+        cnt = 0  # 0に戻す
+ans = max(cnt, ans)
+print(ans)
